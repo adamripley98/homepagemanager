1f6b8013aa960092c026479a1a76155c3dd2af38
Adam Ripley <aripley@wharton.upenn.edu>
2018-04-22 00:26:34 -0400

    cleanup and styling

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 
-// TODO add loading component
-// TODO display errors nicely
-
 class App extends Component {
 
   constructor(props) {
@@ -12,7 +9,7 @@ class App extends Component {
     this.state = {
       success: '',
       error: '',
-      items: ['Dining', 'Laundry', 'News', 'Events', 'Feedback'],
+      items: [],
       options: ['Dining', 'Laundry', 'News', 'Events', 'Feedback'],
       itemNum: 0,
       newItem: '',
@@ -28,8 +25,6 @@ class App extends Component {
   componentDidMount() {
     axios.get('http://localhost:5000/homepage/order')
     .then(resp => {
-      // TODO error check
-      console.log('what is resp.data', resp.data, resp.data.cells);
       if (resp.data.cells && resp.data.cells.length) {
         this.setState({
           items: resp.data.cells,
@@ -40,6 +35,7 @@ class App extends Component {
     .catch(err => {
       this.setState({
         error: 'Error finding homepage content.',
+        success: '',
       })
     })
   }
@@ -50,7 +46,6 @@ class App extends Component {
     })
     .then(resp => {
       if (resp.data.success) {
-        console.log('homepage updated');
         this.setState({
           success: 'Homepage updated.',
           error: ''
@@ -71,44 +66,32 @@ class App extends Component {
   }
 
   handleChangeNewItem(e) {
-    this.setState({
-      newItem: e.target.value,
-    });
+    this.setState({newItem: e.target.value});
   }
 
   addItem() {
     const newItems = this.state.items.slice();
     newItems.push(this.state.items[0]);
-    console.log(newItems);
-    this.setState({
-      items: newItems,
-    });
+    this.setState({items: newItems});
   }
 
   removeItem() {
     const newItems = this.state.items.slice();
     newItems.pop();
-    console.log(newItems);
-    this.setState({
-      items: newItems,
-    });
+    this.setState({items: newItems});
   }
 
   addOption() {
     if (this.state.newItem) {
       const newItems = this.state.items.slice();
       newItems.push(this.state.newItem);
-      this.setState({
-        items: newItems,
-      });
-      console.log('adding option', this.state.newItem);
+      this.setState({items: newItems});
     }
   }
 
   changeItem(e, i){
     const newItems = this.state.items.slice();
     newItems.splice(i, 1, e.target.value);
-    console.log('what is newItems', newItems);
     this.setState({
       items: newItems,
     });
@@ -120,7 +103,7 @@ class App extends Component {
         {
           this.state.items.map((_, index) => (
             <div key={index}>
-              <select value={this.state.items[index]} key={index} onChange={(e) => this.changeItem(e, index)}>
+              <select className="custom-select select" value={this.state.items[index]} key={index} onChange={(e) => this.changeItem(e, index)}>
                 {this.state.options.map((item, i) => (
                   <option value={item} key={i}>{item}</option>
                 ))}
@@ -135,21 +118,32 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Homepage Manager</h1>
-        </header>
-        <p className="App-intro">
-          This web app will be used to control what cells Penn Mobile users will see on the homepage.
-        </p>
-        <div className="container">
-          {this.renderItems()}
-          <p>Add an item to homepage</p>
-          <input type="submit" value="+" className="add" onClick={this.addItem}></input>
-          <p>Remove an item from homepage</p>
-          <input type="submit" value="-" className="delete" onClick={this.removeItem}></input>
-          <br/>
-          <input type="submit" onClick={this.submitForm} value="Submit" className="submit"></input>
+      <div className="container">
+        <div className="card">
+          <div className="card-header">
+            This web app will be used to control what cells Penn Mobile users will see on the homepage.
+          </div>
+          <div className="card-body">
+            {
+              this.state.success &&
+              <div className="alert alert-success" role="alert">
+                {this.state.success}
+              </div>
+            }
+            {
+              this.state.error &&
+              <div className="alert alert-danger" role="alert">
+                {this.state.error}
+              </div>
+            }
+            {this.renderItems()}
+            <div className="buttons">
+              <input type="submit" value="Remove item" className="btn btn-secondary" onClick={this.removeItem}></input>
+              <input type="submit" value="Add item" className="btn btn-secondary" onClick={this.addItem}></input>
+            </div>
+            <div className="line" />
+            <input type="submit" onClick={this.submitForm} value="Submit" className="btn btn-primary"></input>
+          </div>
         </div>
       </div>
     );
